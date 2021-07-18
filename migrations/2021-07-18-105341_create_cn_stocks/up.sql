054f83babc83e1089a7557818edff2304b5b9813
egogg <egogg@163.com>
2021-07-18 19:35:55 +0800

    diesel setup

---
+++ migrations/2021-07-18-105341_create_cn_stocks/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+CREATE TABLE cn_stocks (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+)
---
+++ src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    cn_stocks (id) {
+        id -> Int8,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+    }
+}
