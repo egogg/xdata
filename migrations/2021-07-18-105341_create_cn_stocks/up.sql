08b52ef32dff48ae97e83dbd851623041a7c7d3d
egogg <egogg@163.com>
2021-07-19 14:05:52 +0800

    cn-stocks database design

--- migrations/2021-07-18-105341_create_cn_stocks/up.sql
+++ migrations/2021-07-18-105341_create_cn_stocks/up.sql
@@ -1,6 +1,18 @@
 -- Your SQL goes here
 CREATE TABLE cn_stocks (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  board VARCHAR(255) NOT NULL,
+  code VARCHAR(255) NOT NULL,
+  company_name_cn VARCHAR(255) NOT NULL,
+  company_name_en VARCHAR(255) NOT NULL,
+  company_name_abbr VARCHAR(255) NOT NULL,
+  company_address VARCHAR(255) NOT NULL,
+  company_website VARCHAR(255) NOT NULL,
+  listing_at DATE NOT NULL,
+  a_shares_total BIGINT NOT NULL DEFAULT 0,
+  a_shares_unlimited BIGINT NOT NULL DEFAULT 0,
+  industry VARCHAR(255) NOT NULL,
+  industry_code VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 )
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,18 @@
 table! {
     cn_stocks (id) {
         id -> Int8,
+        board -> Varchar,
+        code -> Varchar,
+        company_name_cn -> Varchar,
+        company_name_en -> Varchar,
+        company_name_abbr -> Varchar,
+        company_address -> Varchar,
+        company_website -> Varchar,
+        listing_at -> Date,
+        a_shares_total -> Int8,
+        a_shares_unlimited -> Int8,
+        industry -> Varchar,
+        industry_code -> Varchar,
         created_at -> Timestamp,
         updated_at -> Timestamp,
     }
